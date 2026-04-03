cc58239a71520d4240c5f651064a3749331c22c3
0xnoxturnee <100279915+0xnoxturnee@users.noreply.github.com>
2026-04-03 07:00:33 +0100

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-03T06:00:25.252Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-03T06:00:26.816Z
+Commit: Fix race condition in async operations
